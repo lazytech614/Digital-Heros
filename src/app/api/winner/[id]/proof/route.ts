6d9d0edcffa7cc89bc55f9f1e32854d0bdec27b6
lazytech614 <derupanjan2021@gmail.com>
2026-03-21 04:03:39 +0530

    updated the params p

--- src/app/api/winner/[id]/proof/route.ts
+++ src/app/api/winner/[id]/proof/route.ts
@@ -2,14 +2,14 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(
-  req: NextRequest,
-  { params }: { params: { id: string } }
+  req: NextRequest, context: { params: Promise<{ id: string }> }
 ) {
   try {
     const { proofUrl } = await req.json();
+    const {id} = await context.params;
 
     const winner = await prisma.winner.update({
-      where: { id: params.id },
+      where: { id },
       data: {
         proofUrl,
         status: "PENDING", // stays pending until admin verifies
